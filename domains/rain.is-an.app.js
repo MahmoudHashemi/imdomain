46ebd3ef8dade8be042aefa3d58d1153698eeef6
Mahmoud Hashemi <105546951+MahmoudHashemi@users.noreply.github.com>
2024-04-27 14:22:26 +0330

    Update rain.is-an.app.js

--- domains/rain.is-an.app.js
+++ domains/rain.is-an.app.js
@@ -1,27 +1,13 @@
 addSubDomain({
-  description: 'rainproxy project', // describe your project in this field
-  domain: 'is-an.app', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
-  subdomain: 'rain', // desired subdomain name
+  description: 'rainproxy project',
+  domain: 'is-an.app', 
+  subdomain: 'rain', 
   owner: {
     repo: 'https://github.com/MahmoudHashemi/imdomain',
     email: 'info.jibisite@gmail.com',
   },
   record: {
-    CNAME: 'px-2087.rainlist.site', 
+    CNAME: 'cloud1-git.rainlist.site', 
   },
-  proxy: true, // disable Cloudflare proxy (with is enabled by default). In this case, your origin server
-                  // should provide valid a SSL certificate and protection CF will be disabled
-  // nested: [{ // in addition, you may define the required nested subdomains
-  //   subdomain: 'foo',
-  //   record: {
-  //     CNAME: '...',
-  //   },
-  //   proxy: false,
-  // }, {
-  //   subdomain: 'bar',
-  //   record: {
-  //     A: ['...'],
-  //   },
-  //   proxy: true,
-  // }]
+  proxy: true, 
 })
